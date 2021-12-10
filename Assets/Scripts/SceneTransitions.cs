8f92ab2ebe6dcbd0b31617a0f34601e6409b0b9a
TheMagicalFishTaco <50703929+TheMagicalFishTaco@users.noreply.github.com>
2021-12-10 20:20:15 +0800

    Put some comments on my code

--- Assets/Scripts/SceneTransitions.cs
+++ Assets/Scripts/SceneTransitions.cs
@@ -6,18 +6,22 @@ using UnityEngine.SceneManagement;
 //this script handles transitions between the different scenes
 public class SceneTransitions : MonoBehaviour
 {
+    //Loads Stage 1 when you click Start Game on the Main Menu
     public void StartGame()
     {
         SceneManager.LoadScene("Stage 1");
     }
+    //Returns to the Main Menu
     public void returnToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
+    //Loads the end screen where you can select whether to move to next level, restart level, or go to main menu
     public static void LevelEnd()
     {
         SceneManager.LoadScene("LevelSelect");
     }
+    //This is when you click on next level at the end screen
     public static void LoadLevel()
     {
         string currLevel = StaticFunction.getCurrentLevel();
@@ -36,6 +40,7 @@ public class SceneTransitions : MonoBehaviour
             SceneManager.LoadScene("MainMenu");
         }
     }
+    //This is when you click on restart level on the end screen
     public static void RestartLevel()
     {
         string currLevel = StaticFunction.getCurrentLevel();
@@ -53,6 +58,7 @@ public class SceneTransitions : MonoBehaviour
         }
         SceneManager.LoadScene(currLevel);
     }
+    //Exits the game
     public void ExitGame()
     {
         Application.Quit();
