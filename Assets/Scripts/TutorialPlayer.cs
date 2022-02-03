d2db795b3b11aac2a7494b8e4d2a79b0f4aa0b73
Li Niko <hannah.asis@obf.ateneo.edu>
2022-02-03 10:41:03 +0800

    Tutorials done

--- Assets/Scripts/TutorialPlayer.cs
+++ Assets/Scripts/TutorialPlayer.cs
@@ -28,11 +28,34 @@ public class TutorialPlayer : MonoBehaviour
     {
         new Dialogue("Greetings, and welcome to Digital Footprints, where your privacy is our priority."),
         new Dialogue("For your first day, we will be letting you handle low-priority customers."),
+        new Dialogue("These are customers who only allow us to look through their public profiles."),
         new Dialogue("Please use this time to familiarize yourself with the company’s software and workflow."),
         new Dialogue("If you have any questions to ask the client, you may right-click the item of concern and talk to them about it."),
         new Dialogue("Do be warned that sending false information to the customer will warrant a penalty."),
-        new Dialogue("So make sure to double-check your report before you SUBMIT!!"),
+        new Dialogue("So make sure to double-check your report before you [SUBMIT]."),
         new Dialogue("We hope that your time with us will be informative and fulfilling, good luck!", true)
+    };
+    
+    private Dialogue[] stageTwo = new Dialogue[]
+    {
+        new Dialogue("Hello, again! It seems like you're doing really well in this company."),
+        new Dialogue("So we've decided to let you handle medium-priority customers."),
+        new Dialogue("These are customers who allow us to look into their privacy settings."),
+        new Dialogue("More details on proper Privacy Settings can be found in your Company Standards."),
+        new Dialogue("Also, you now have access to more social media sites. Make sure to check everything on both sites!"),
+        new Dialogue("As always, make sure to double-check your report before you [SUBMIT]."),
+        new Dialogue("Good luck!", true)
+    };
+
+    private Dialogue[] stageThree = new Dialogue[]
+    {
+        new Dialogue("Great news! You've been doing so well in this company that we've decided to give you access to high-priority customers."),
+        new Dialogue("These are customers who allow us to look into their passwords."),
+        new Dialogue("It's a good thing we had you sign that NDA before joining the company! Haha!"),
+        new Dialogue("There are more details on proper Password Strength in your Company Standards."),
+        new Dialogue("Also, you now have access to more social media sites. Make sure to check everything on all sites!"),
+        new Dialogue("As always, make sure to double-check your report before you [SUBMIT]."),
+        new Dialogue("Good luck!", true)
     };
 
     // Start is called before the first frame update
@@ -52,6 +75,14 @@ public class TutorialPlayer : MonoBehaviour
             if (SceneManager.GetActiveScene().name.Equals("Stage 1"))
             {
                 StartCoroutine(runStage(stageOne));
+            }
+            else if (SceneManager.GetActiveScene().name.Equals("Stage 2"))
+            {
+                StartCoroutine(runStage(stageTwo));
+            }
+            else if (SceneManager.GetActiveScene().name.Equals("Stage 3"))
+            {
+                StartCoroutine(runStage(stageThree));
             }
         }
     }
